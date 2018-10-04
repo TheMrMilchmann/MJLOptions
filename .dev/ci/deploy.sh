21b97477ca4761040257a1c93e90a18eb14d7053
Leon Linhart <themrmilchmann@gmail.com>
2018-10-04 12:37:20 +0200

    build: set executable bits for deploy.sh

--- .dev/ci/deploy.sh
+++ .dev/ci/deploy.sh
@@ -6,8 +6,9 @@ JDOC_JDK="openjdk11"
 BRANCH="master"
 
 set -e
+./gradlew check --info -S --parallel -Psnapshot
 
-if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] && [ "$TRAVIS_JDK_VERSION" == "$JDK" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "$BRANCH" ]; then
     if [ "$TRAVIS_JDK_VERSION" == "$JDK" ]; then
         # Upload snapshot artifacts to OSSRH.
         echo -e "[deploy.sh] Publishing snapshots...\n"
@@ -19,22 +20,33 @@ if [ "$TRAVIS_REPO_SLUG" == "$SLUG" ] && [ "$TRAVIS_JDK_VERSION" == "$JDK" ] && 
         # Upload JavaDoc to GH-Pages.
         echo -e "[deploy.sh] Publishing documentation...\n"
         mkdir out
-        cd out
-        git init
-        git config user.name "Deployment Bot"
-        git config user.email "deploy@travis-ci.org"
 
-        if [ "$TRAVIS_TAG" =~ ^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$ ]; then
+        if [ "$TRAVIS_TAG" == '^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$' ]; then
+            ./gradlew javadoc --parallel -Prelease
+            cd out
+            git init
+            git config user.name "Deployment Bot"
+            git config user.email "deploy@travis-ci.org"
+            git pull -q "https://github.com/$SLUG.git" gh-pages
+            mkdir -p ./docs/${TRAVIS_TAG}
             cp -r ../build/docs/javadoc/* ./docs/${TRAVIS_TAG}
             COMMIT_MSG="feat(ci): $TRAVIS_TAG release documentation"
         else
+            ./gradlew javadoc --parallel -Psnapshot
+            cd out
+            git init
+            git config user.name "Deployment Bot"
+            git config user.email "deploy@travis-ci.org"
+            git pull -q "https://github.com/$SLUG.git" gh-pages
+            rm -rf ./docs/snapshot/
+            mkdir -p ./docs/snapshot/
             cp -r ../build/docs/javadoc/* ./docs/snapshot/
             COMMIT_MSG="feat(ci): snapshot documentation for build $TRAVIS_BUILD_NUMBER ($TRAVIS_COMMIT)"
         fi
 
         git add .
         git commit -m "$COMMIT_MSG"
-        git push --quiet "https://${GH_TOKEN}@github.com/$SLUG" master:gh-pages
+        git push -q "https://${GH_TOKEN}@github.com/$SLUG" master:gh-pages
         echo -e "[deploy.sh] Published documentation.\n"
     fi
 fi
